32e8f945b188856f4990da256b401b3b828e408c
rohancdavid <rohancdavid@gmail.com>
2024-07-27 14:12:07 -0400

    final

--- rcd_encode.py
+++ rcd_encode.py
@@ -14,3 +14,29 @@ def decode(password): # decode function added by Samantha Wong
         x = int(password[i]) -3
         new_string += str(x)
     return new_string
+
+
+def main():
+    password = None
+    encoded_str = None
+    while True:
+        #menu
+        print("Menu\n-------------")
+        print("1. Encode\n2. Decode\n3. Quit\n")
+        #option
+        option = int(input("Please enter an option: "))
+        if option == 1: #encode
+            password = input("Please enter your password to encode: ")
+            encoded_pw = encode(password)
+            print("Your password has been encoded and stored!\n")
+
+        elif option == 2: #decode
+            decode(encoded_pw)
+            print(f"The encoded password is {encoded_pw}, and the original password is {password}.\n")
+
+        elif option == 3: #quit
+            break
+
+
+if __name__ == "__main__":
+    main()
